fb6ce0c06981445e0f5fa70bf7c969069f87f2d7
Gauf10 <gauf10@gmail.com>
2026-09-03 15:41:14 -0300

    cache-bust

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,13 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'google-fonts-cache', expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 } },
+          },
+        ],
       },
     }),
   ],
